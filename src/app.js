495ec33ee4483b18bc17e8aae7aaac89b5249679
adityabuildsmachines-art <adityabuildsmachines@gmail.com>
2026-02-10 10:25:41 +0530

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CognitiveStyleAssessment from './CognitiveStyleAssessment';
+
+function App() {
+  return (
+    <div className="App">
+      <CognitiveStyleAssessment />
+    </div>
+  );
+}
+
+export default App;
